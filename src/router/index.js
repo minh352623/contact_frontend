58a180c15edaa33b714a90c6727286a015514b57
CongMinh <congminh352623@gmail.com>
2023-02-01 15:43:53 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,17 @@ const routes = [
     name: "contactbook",
     component: ContactBook,
   },
+  // {
+  //   path: "/contact/add",
+  //   name: "contact.add",
+  //   component: ContactAdd,
+  // },
+  {
+    path: "/contacts/:id",
+    name: "contact.edit",
+    component: () => import("@/views/ContactEdit.vue"),
+    props: true, // Truyền các biến trong $route.params vào làm props
+  },
   {
     path: "/:pathMatch(.*)*",
     name: "notfound",
